12de0184738aee76a23e40b2a97a55c09fd7d6ec
Hurray0 <i@ihurray.com>
2014-11-06 08:27:55 +0800

    【server】建立server

---
+++ Server/src/MainPart/Server.java
@@ -0,0 +1,21 @@
+/**
+ * @Author hurray
+ * @Part 
+ * @Note 
+ * @Encoding UTF-8 
+ * @Date 2014-11-06 08:22:08
+ * @Copyright Hurray@BUPT
+ * @MainPage http://www.ihurray.com
+ * 
+ */
+
+package MainPart;
+
+/**
+ * @Author hurray
+ * @Class Server
+ */
+public class Server {
+    public static void main(String[] args)
+    {}
+}
